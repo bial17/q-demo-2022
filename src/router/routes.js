65b38896fc6a6c1d66fa973b3ef7783481fabafb
alexandre.bilat <alexandre.bilat@divcom.ch>
2022-04-27 11:47:40 +0200

    premier commit

---
+++ src/router/routes.js
@@ -0,0 +1,22 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'ex1page', component: () => import('pages/Exercice1Page.vue') },
+      { path: 'ex2', component: () => import('pages/Exercice2Page.vue') },
+      { path: 'ex1', component: () => import('pages/Exercice1.vue') }
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes
